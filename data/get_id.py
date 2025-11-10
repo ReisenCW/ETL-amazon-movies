d0cc338adc44e713363e18849893221578bd2d2c
ReisenCW <814568436@qq.com>
2025-11-10 09:58:49 +0800

    实现最基本的爬虫和信息提取

---
+++ data/get_id.py
@@ -0,0 +1,17 @@
+# 从原始数据集读取所有product_id（去重）
+def get_all_product_ids():
+    product_ids = set()
+    with open("../data/movies.txt", "r", encoding="latin-1") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("product/productId:"):
+                pid = line.split(": ")[1]
+                product_ids.add(pid)
+    return list(product_ids)
+
+if __name__ == "__main__":
+    # 将list写入文件product_ids.txt，每行一个
+    product_ids = get_all_product_ids()
+    with open("../data/product_ids.txt", "w", encoding="utf-8") as f:
+        for pid in product_ids:
+            f.write(f"{pid}\n")
